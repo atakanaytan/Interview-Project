2fee5a27540f986e1f6d950aa74f2fda643c1675
atakanaytan <aytan.atakan2@gmail.com>
2020-03-14 04:09:00 +0300

    Relations between tables have connected

--- src/main/java/wardrobe/wardrobe/domain/Category.java
+++ src/main/java/wardrobe/wardrobe/domain/Category.java
@@ -12,29 +12,23 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-
     @NotBlank(message = "Category name is required")
     private String categoryName;
 
-
     public Category( ) {
     }
-
 
     public long getId() {
         return id;
     }
 
-
     public void setId(long id) {
         this.id = id;
     }
 
-
     public String getName() {
         return categoryName;
     }
-
 
     public void setName(String categoryName) {
         this.categoryName = categoryName;
--- src/main/java/wardrobe/wardrobe/domain/Product.java
+++ src/main/java/wardrobe/wardrobe/domain/Product.java
@@ -14,14 +14,11 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
-
     @NotBlank(message = "Project name is required")
     private String productName;
 
-
     @Size(max=255, message ="Description can be max 255 character long.")
     private String projectDescription;
-
 
     @NotBlank(message = "Product price is required")
     private double productPrice;
@@ -30,70 +27,55 @@ public class Product {
     @JoinColumn(name = "category_id")
     private Category category;
 
-
     private int quantity;
-
 
     private Date create_At;
 
-
     private Date update_At;
-
 
     public Product() {
 
     }
 
-
     public Long getId() {
         return Id;
     }
-
 
     public void setId(Long id) {
         Id = id;
     }
 
-
     public String getProductName() {
         return productName;
     }
-
 
     public void setProductName(String productName) {
         this.productName = productName;
     }
 
-
     public String getProjectDescription() {
         return projectDescription;
     }
-
 
     public void setProjectDescription(String projectDescription) {
         this.projectDescription = projectDescription;
     }
 
-
     public double getProductPrice() {
         return productPrice;
     }
-
 
     public void setProductPrice(double productPrice) {
         this.productPrice = productPrice;
     }
 
-
     public int getProductStock() {
         return quantity;
     }
 
-
     public void setProductStock(int quantity) {
         this.quantity = quantity;
     }
-
 
     @JsonFormat(pattern = "yyyy-mm-dd")
     @Column (updatable = false)
@@ -101,17 +83,14 @@ public class Product {
         return create_At;
     }
 
-
     @JsonFormat(pattern = "yyyy-mm-dd")
     public void setCreate_At(Date create_At) {
         this.create_At = create_At;
     }
 
-
     public Date getUpdate_At() {
         return update_At;
     }
-
 
     public void setUpdate_At(Date update_At) {
         this.update_At = update_At;
